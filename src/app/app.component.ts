35faa67ff0f91d1c131523ed90a74c3b7837ada7
matfio <mtt.fiorio@gmail.com>
2022-07-02 17:16:28 +0200

    added more logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   newMember = '';
   members: string[] = [];
   errorMessage: string = '';
+  teamsAmount: number | '' = '';
+  teams: string[][] = [];
 
   //On button press, add the newMember value into the members array
   addMember() {
@@ -25,5 +27,45 @@ export class AppComponent {
   //On input update the newMember value with the input value
   onInput(member: string) {
     this.newMember = member;
+  }
+
+  //On input update the teamsAmount value with the input value
+  onTeamsAmountInput(amount: string) {
+    this.teamsAmount = Number(amount);
+  }
+
+  generateTeams() {
+    if (!this.teamsAmount || this.teamsAmount <= 0) {
+      this.errorMessage = 'Please enter a valid amount';
+      return;
+    }
+
+    if (this.members.length < this.teamsAmount) {
+      this.errorMessage = 'Too many teams for too few people';
+      return;
+    }
+
+    this.errorMessage = '';
+    const allMembers = [...this.members];
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.teamsAmount; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if (!member) {
+          break;
+        }
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    this.members = [];
+    this.teamsAmount = 0;
   }
 }
