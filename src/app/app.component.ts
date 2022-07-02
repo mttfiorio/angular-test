74da215f0c5f05a0c0d24bef34b85ee1566030ff
matfio <mtt.fiorio@gmail.com>
2022-07-02 11:29:51 +0200

    added error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMember = '';
   members: string[] = [];
+  errorMessage: string = '';
 
   //On button press, add the newMember value into the members array
   addMember() {
+    if (!this.newMember) {
+      this.errorMessage = 'Please enter a new member name';
+      return;
+    }
+
+    this.errorMessage = '';
     this.members.push(this.newMember);
+    this.newMember = '';
   }
 
   //On input update the newMember value with the input value
