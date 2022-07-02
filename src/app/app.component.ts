96aadd239a06884ec678ca13e7c58a15a01d4484
matfio <mtt.fiorio@gmail.com>
2022-07-02 11:21:50 +0200

    adding structura directive logic and input handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-test';
+  newMember = '';
+  members: string[] = [];
+
+  //On button press, add the newMember value into the members array
+  addMember() {
+    this.members.push(this.newMember);
+  }
+
+  //On input update the newMember value with the input value
+  onInput(member: string) {
+    this.newMember = member;
+  }
 }
